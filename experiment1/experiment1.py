0463b3417f58619c21317522dbe9cdf2626bd0aa
posix <posixnnja@gmail.com>
2019-03-13 16:46:30 -0400

    -Added comments to experiment1.py

--- experiment1/experiment1.py
+++ experiment1/experiment1.py
@@ -3,16 +3,17 @@
 import os
 
 
+# Getting all the data from the trace file into variables.
 class C:
     def __init__(self, line):
         contents = line.split()
-        self.event = contents[0]
+        self.event_type = contents[0]
         self.time = float(contents[1])
         self.from_node = contents[2]
         self.to_node = contents[3]
-        self.pkt_type = contents[4]
-        self.pkt_size = int(contents[5])
-        self.seq_num = contents[10]
+        self.packet_type = contents[4]
+        self.packet_size = int(contents[5])
+        self.sequence_num = contents[10]
 
 
 # Generate trace file
@@ -20,14 +21,16 @@ for tcp_var in ['Tahoe', 'Reno', 'Newreno', 'Vegas']:
     for cbr_rate in range(1, 13):
         os.system("/course/cs4700f12/ns-allinone-2.35/bin/ns " + "experiment1.tcl " + tcp_var + " " + str(cbr_rate))
 
+# Data files used for graph generation.
 f1 = open('exp1_data/1_throughput.dat', 'w')
 f2 = open('exp1_data/1_droprate.dat', 'w')
 f3 = open('exp1_data/1_latency.dat', 'w')
 
+# CBR rate varying between 1 and 12mb until it reaches bottleneck capacity.
 for cbr_rate in range(1, 13):
-    str_throughput = ''
-    str_droprate = ''
-    str_latency = ''
+    throughput = ''
+    droprate = ''
+    latency = ''
 
     for tcp_var in ['Tahoe', 'Reno', 'Newreno', 'Vegas']:
         # Open the throughput files for TCP variants and rates and calculate the throughput.
@@ -40,33 +43,40 @@ for cbr_rate in range(1, 13):
         recvdSize = 0
         for line in lines:
             entry = C(line)
-            if entry.pkt_type in ['tcp', 'ack']:
-                if entry.event == "+" and entry.from_node == "0":
+            if entry.packet_type in ['tcp', 'ack']:
+                # Calculate period of time to send packets from node 1.
+                if entry.event_type == "+" and entry.from_node == "0":
                     if entry.time < start_time:
                         start_time = entry.time
-                if entry.event == "r":
-                    recvdSize += entry.pkt_size * 8
+                # Calculate size of packets in that period.
+                if entry.event_type == "r":
+                    recvdSize += entry.packet_size * 8
                     end_time = entry.time
-        str_throughput = str_throughput + '\t' + str(recvdSize / (end_time - start_time) / (1024 * 1024))
+        # Final throughput data to be written into file.
+        throughput = throughput + '\t' + str(recvdSize / (end_time - start_time) / (1024 * 1024))
 
         # Open the droprate files for TCP Variants and rates and calculate the droprate.
         filename = "exp1_output/experiment1_" + tcp_var + "_" + str(cbr_rate) + ".out"
         f = open(filename)
         lines = f.readlines()
         f.close()
-        sendNum = recvdNum = 0
+        sendNum = 0
+        recvdNum = 0
         for line in lines:
             entry = C(line)
             # Packet type is tcp and it's acks within which we need to count sent - received / sent
-            if entry.pkt_type in ["tcp", "ack"]:
-                if entry.event == "+":
+            if entry.packet_type in ["tcp", "ack"]:
+                # Packets in the queue
+                if entry.event_type == "+":
                     sendNum += 1
-                if entry.event == "r":
+                # Packets received
+                if entry.event_type == "r":
                     recvdNum += 1
         if sendNum == 0:
-            str_droprate = '0'
+            droprate = '0'
         else:
-            str_droprate = str_droprate + '\t' + str(float(sendNum - recvdNum) / float(sendNum))
+            # Final droprate to be written into file.
+            droprate = droprate + '\t' + str(float(sendNum - recvdNum) / float(sendNum))
 
         # Open the latency files for TCP Variants and rates and calculate the delay.
         filename = "exp1_output/experiment1_" + tcp_var + "_" + str(cbr_rate) + ".out"
@@ -80,13 +90,13 @@ for cbr_rate in range(1, 13):
 
         for line in lines:
             entry = C(line)
-            if entry.pkt_type in ["tcp", "ack"]:
-                if entry.from_node == "0" and entry.event == "+":
+            if entry.packet_type in ["tcp", "ack"]:
+                if entry.from_node == "0" and entry.event_type == "+":
                     # tracking start time of all packets originating from node 0 that are queueing
-                    start_dict[entry.seq_num] = entry.time
-                elif entry.to_node == "0" and entry.event == "r":
+                    start_dict[entry.sequence_num] = entry.time
+                elif entry.to_node == "0" and entry.event_type == "r":
                     # tracking end time of all packets returning ACKs at node 0
-                    end_dict[entry.seq_num] = entry.time
+                    end_dict[entry.sequence_num] = entry.time
 
         for key in start_dict:
             if key in end_dict.keys():
@@ -96,12 +106,13 @@ for cbr_rate in range(1, 13):
                     num_packets += 1
 
         if num_packets == 0:
-            str_latency = '0'  # CHECK -- str latency
-        str_latency = str_latency + '\t' + str(total_duration / num_packets * 1000)
+            latency = '0'
+        # Final latency to be written into file.
+        latency = latency + '\t' + str(total_duration / num_packets * 1000)
 
-    f1.write("Rate: " + str(cbr_rate) + " Throughputs: " + str_throughput + '\n')
-    f2.write("Rate: " + str(cbr_rate) + " Droprates: " + str_droprate + '\n')
-    f3.write("Rate: " + str(cbr_rate) + " Latencies: " + str_latency + '\n')
+    f1.write("Rate: " + str(cbr_rate) + " Throughputs: " + throughput + '\n')
+    f2.write("Rate: " + str(cbr_rate) + " Droprates: " + droprate + '\n')
+    f3.write("Rate: " + str(cbr_rate) + " Latencies: " + latency + '\n')
 
 f1.close()
 f2.close()
